f6ec035df533a968a51ab8cd1f455bc108dab62c
soumyadip007 <soumyadip.note@gmail.com>
2019-05-23 20:42:16 +0530

    Update Student Imported

---
+++ Hibernate/src/com/learn/hibernate/control/UpdateStudentDemo.java
@@ -0,0 +1,81 @@
+package com.learn.hibernate.control;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.learn.hibernate.entity.Student;
+
+public class UpdateStudentDemo {
+
+	public static void main(String[] args) {
+		// Create Session Factory
+			SessionFactory factory=new Configuration().
+							configure("hibernate.cfg.xml").
+							addAnnotatedClass(Student.class).
+							buildSessionFactory();
+			
+		
+			//Create session
+			
+			Session session = ((SessionFactory) factory).getCurrentSession();
+			
+			try {
+				
+			
+				//Start a transaction
+				session.beginTransaction();
+				
+
+				//query student 
+				List<Student> obj=session.createQuery("from Student").list();
+			
+				System.out.println("\nAll students :\n");
+				displatStudents(obj);
+				
+				
+				//query Student : last_name="Chowdhury"
+				obj=session.createQuery("from Student s where s.last_name='Chowdhury'").list();  //S is the SQL object
+				
+				System.out.println("\nStudents last_name=Chowdhury:\n");
+				
+				displatStudents(obj);
+				
+				//query Student : Like
+				obj=session.createQuery("from Student s where s.last_name like '%Chowd%'").list();  //S is the SQL object
+				
+				System.out.println("\nStudents last_name in like:\n");
+				
+				displatStudents(obj);
+				
+				//query Student : OR
+				obj=session.createQuery("from Student s where s.last_name='Chowdhury' or s.first_name='Reshmi'").list();  //S is the SQL object
+				
+				System.out.println("\nStudents first_name OR last_name :\n");
+
+				displatStudents(obj);
+				
+				//Commit transaction
+				session.getTransaction().commit();  
+
+				System.out.println("Done");
+				
+			}
+			finally {
+				factory.close();
+			}
+	
+	}
+
+	
+	private static void displatStudents(List<Student> obj) {
+		for(Student temp: obj)
+		{
+			System.out.println(temp);
+		}
+	}
+}
+	
+
