bcbf3e311c5b4b3c2f4608f006d1e05f3983adda
soumyadip007 <soumyadip.note@gmail.com>
2019-05-23 16:33:41 +0530

    Class & Annotation added

---
+++ Hibernate/src/com/learn/hibernate/entity/Student.java
@@ -0,0 +1,29 @@
+package com.learn.hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Student")
+public class Student {
+
+	@Id        //Primary key
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String first_name;
+	
+	@Column(name="last_name")
+	private String last_name;
+	
+	@Column(name="email")
+	private String email;
+	
+	public Student()
+	{
+		
+	}
+}
