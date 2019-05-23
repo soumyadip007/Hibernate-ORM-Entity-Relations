4ec34c44c4818a3f560a385aa4fc7ff7abf28f80
soumyadip007 <soumyadip.note@gmail.com>
2019-05-23 16:35:27 +0530

    Getters & Setters, ToString method added

--- Hibernate/src/com/learn/hibernate/entity/Student.java
+++ Hibernate/src/com/learn/hibernate/entity/Student.java
@@ -16,14 +16,57 @@ public class Student {
 	@Column(name="first_name")
 	private String first_name;
 	
+
 	@Column(name="last_name")
 	private String last_name;
 	
 	@Column(name="email")
 	private String email;
 	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirst_name() {
+		return first_name;
+	}
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+
+	public String getLast_name() {
+		return last_name;
+	}
+
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+	@Override     //For debugging
+	public String toString() {
+		return "Student [id=" + id + ", first_name=" + first_name + ", last_name=" + last_name + ", email=" + email
+				+ "]";
+	}
+	
 	public Student()
 	{
 		
 	}
+	
 }
