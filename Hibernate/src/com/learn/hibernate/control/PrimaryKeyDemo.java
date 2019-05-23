24ab24dc98cb862d29f1590d163426d080ba107a
soumyadip007 <soumyadip.note@gmail.com>
2019-05-23 18:57:24 +0530

    Primary Key Control

---
+++ Hibernate/src/com/learn/hibernate/control/PrimaryKeyDemo.java
@@ -0,0 +1,55 @@
+package com.learn.hibernate.control;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.learn.hibernate.entity.Student;
+
+public class PrimaryKeyDemo {
+
+	public static void main(String[] args) {
+		// Create Session Factory
+			SessionFactory factory=new Configuration().
+							configure("hibernate.cfg.xml").
+							addAnnotatedClass(Student.class).
+							buildSessionFactory();
+			
+		
+			//Create session
+			
+			Session session = ((SessionFactory) factory).getCurrentSession();
+			
+			try {
+				
+				//Create 3 Student Object
+				System.out.println("Creating new student object");
+				Student obj=new Student("Soumyadip","Chowdhury","Soumyadip.note@gmail.com");
+				Student obj1=new Student("Soumyadip","Chowdhury","Soumyadip.cmp@gmail.com");
+				Student obj2=new Student("Soumyadip","Chowdhury","Soumyadip123@gmail.com");
+				
+				
+				//Start a transaction
+				session.beginTransaction();
+				
+				
+				//Save the student object
+				System.out.println("Saving the student");
+				session.save(obj);
+				session.save(obj1);
+				session.save(obj2);
+				
+				//Commit transaction
+				session.getTransaction().commit();
+
+				System.out.println("Done");
+				
+			}
+			finally {
+				factory.close();
+			}
+	
+	}
+}
+	
+
