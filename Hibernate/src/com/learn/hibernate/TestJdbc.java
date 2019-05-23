bd4b59b8c5380075eee41515b58e56a02ecbd4e6
soumyadip007 <soumyadip.note@gmail.com>
2019-05-23 16:07:37 +0530

    JDBC Connection established

---
+++ Hibernate/src/com/learn/hibernate/TestJdbc.java
@@ -0,0 +1,25 @@
+package com.learn.hibernate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestJdbc {
+
+	
+	public static void main(String args[]) throws SQLException
+	{
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			String url="jdbc:mysql://localhost:3306/hackathon";
+		
+			Connection con=(Connection) DriverManager.getConnection(url,"root","");
+			
+			System.out.println("Successfull !");
+		
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	}
+	}
+}
