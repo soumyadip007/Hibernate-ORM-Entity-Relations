e9075a1ed50f1cab916e8193826a5c4b9d07b13b
soumyadip007 <soumyadip.note@gmail.com>
2019-05-24 11:01:58 +0530

    Getters/Setters , Mapping, ToString etc Added

--- Hibernate/src/com/learn/hibernate/relation/entity/Instructor.java
+++ Hibernate/src/com/learn/hibernate/relation/entity/Instructor.java
@@ -1,14 +1,17 @@
 package com.learn.hibernate.relation.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="instructor_detail")
+@Table(name="instructor")
 public class Instructor {
 
 	@Id        //Primary key
@@ -26,5 +29,63 @@ public class Instructor {
 	@Column(name="email")
 	private String email;
 	
+	@OneToOne (cascade=CascadeType.ALL)
+	@JoinColumn(name="instructor_detail")
+	private InstructorDetail obj;
+
+	public Instructor(String firstname, String lastname, String email) {
+	//	super();
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public InstructorDetail getObj() {
+		return obj;
+	}
+
+	public void setObj(InstructorDetail obj) {
+		this.obj = obj;
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Instructor [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", email=" + email
+				+ ", obj=" + obj + "]";
+	}
 
 }
