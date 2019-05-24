0c554176b4a809477f3268fe9a64718c31ff7821
soumyadip007 <soumyadip.note@gmail.com>
2019-05-24 13:48:27 +0530

    InntructorDetail  @MappedBy added (BiDirectional)

--- Hibernate/src/com/hibernate/OneToOne/BiDirection/entity/InstructorDetail.java
+++ Hibernate/src/com/hibernate/OneToOne/BiDirection/entity/InstructorDetail.java
@@ -1,10 +1,12 @@
 package com.hibernate.OneToOne.BiDirection.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -23,6 +25,21 @@ public class InstructorDetail {
 	@Column(name="hobby")
 	private String hobby;
 	
+	
+	//Add new field for instructor Class (also Getter & Setter)
+	
+	@OneToOne(mappedBy="instuctor_Detail", cascade=CascadeType.ALL)
+	private Instructor instructor; 
+
+	
+	
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
+	}
 
 	public int getId() {
 		return id;
