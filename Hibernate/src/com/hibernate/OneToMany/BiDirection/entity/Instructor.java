434366b573e785c906fe6e4354ea477577ea97eb
soumyadip007 <soumyadip.note@gmail.com>
2019-05-24 18:01:59 +0530

    EGEAR appiled

--- Hibernate/src/com/hibernate/OneToMany/BiDirection/entity/Instructor.java
+++ Hibernate/src/com/hibernate/OneToMany/BiDirection/entity/Instructor.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -37,7 +38,8 @@ public class Instructor {
 	@JoinColumn(name="instructor_detail_id")
 	private InstructorDetail instructorDetail;
 
-	@OneToMany(mappedBy="instructor",
+	@OneToMany(fetch=FetchType.EAGER,	
+				mappedBy="instructor",
 				cascade= {CascadeType.PERSIST ,CascadeType.MERGE, 
 						CascadeType.DETACH, CascadeType.REFRESH})  //Reffers to the instructor property to the course class
 	private List<Course> courses; 
--- Hibernate/src/com/hibernate/fetch/EgerVsLazy/EgerVsLazyDemo.java
+++ Hibernate/src/com/hibernate/fetch/EgerVsLazy/EgerVsLazyDemo.java
@@ -38,9 +38,7 @@ public class EgerVsLazyDemo {
 				System.out.println("Retrived object of InstructorDetail :"+obj);
 				
 
-				System.out.println("Courses :"+obj.getCourses());
-			
-				
+				System.out.println("Courses :"+obj.getCourses());				
 				
 				
 				
@@ -60,9 +58,9 @@ public class EgerVsLazyDemo {
 }
 	
 
-/*Hibernate: select instructor0_.id as id1_1_0_, instructor0_.email as email2_1_0_, instructor0_.first_name as first_na3_1_0_, instructor0_.instructor_detail_id as instruct5_1_0_, instructor0_.last_name as last_nam4_1_0_, instructor1_.id as id1_2_1_, instructor1_.hobby as hobby2_2_1_, instructor1_.youtube_channel as youtube_3_2_1_ from instructor instructor0_ left outer join instructor_detail instructor1_ on instructor0_.instructor_detail_id=instructor1_.id where instructor0_.id=?
+/*
+ Hibernate: select instructor0_.id as id1_1_0_, instructor0_.email as email2_1_0_, instructor0_.first_name as first_na3_1_0_, instructor0_.instructor_detail_id as instruct5_1_0_, instructor0_.last_name as last_nam4_1_0_, courses1_.instructor_id as instruct3_0_1_, courses1_.id as id1_0_1_, courses1_.id as id1_0_2_, courses1_.instructor_id as instruct3_0_2_, courses1_.title as title2_0_2_, instructor2_.id as id1_2_3_, instructor2_.hobby as hobby2_2_3_, instructor2_.youtube_channel as youtube_3_2_3_ from instructor instructor0_ left outer join course courses1_ on instructor0_.id=courses1_.instructor_id left outer join instructor_detail instructor2_ on instructor0_.instructor_detail_id=instructor2_.id where instructor0_.id=?
 Retrived object of InstructorDetail :Instructor [id=1, firstname=Soumyadip, lastname=Chowdhury, email=Soumyadip.note@gmail.com, InstructorDetail=InstructorDetail [id=1, youtube_channel=https://github.com/soumyadip007, hobby=Coding]]
-Hibernate: select courses0_.instructor_id as instruct3_0_0_, courses0_.id as id1_0_0_, courses0_.id as id1_0_1_, courses0_.instructor_id as instruct3_0_1_, courses0_.title as title2_0_1_ from course courses0_ where courses0_.instructor_id=?
-Courses :[com.hibernate.OneToMany.UniDirection.entity.Course@1ee29c84, com.hibernate.OneToMany.UniDirection.entity.Course@7c8326a4]
+Courses :[com.hibernate.OneToMany.BiDirection.entity.Course@69eb86b4, com.hibernate.OneToMany.BiDirection.entity.Course@5173200b]
 Done
 */
